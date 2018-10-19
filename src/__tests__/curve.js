afaaf033dcd29ddf27db1a2559a7744025a87f13
Jacob Quant <jacobq@gmail.com>
2018-10-19 13:00:13 -0500

    WIP: Refactor fourParamEq test

--- src/__tests__/curve.js
+++ src/__tests__/curve.js
@@ -4,26 +4,6 @@ import levenbergMarquardt from '..';
 
 expect.extend({ toBeDeepCloseTo });
 
-function fourParamEq([a, b, c, d]) {
-  return (t) => a + (b - a) / (1 + Math.pow(c, d) * Math.pow(t, -d));
-}
-
-const data = {
-  x: [
-    9.22e-12,
-    5.53e-11,
-    3.32e-10,
-    1.99e-9,
-    1.19e-8,
-    7.17e-8,
-    4.3e-7,
-    0.00000258,
-    0.0000155,
-    0.0000929
-  ],
-  y: [7.807, -3.74, 21.119, 2.382, 4.269, 41.57, 73.401, 98.535, 97.059, 92.147]
-};
-
 describe('Example problems', () => {
   it('Should fit 2*sin(3*t)', () => {
     const getFunction = ([a, b]) => (t => a * Math.sin(b * t));
@@ -63,42 +43,58 @@ describe('Example problems', () => {
     const actual = levenbergMarquardt(data, getFunction, options);
     expect(actual.parameterValues).toBeDeepCloseTo(exactParameters, 3);
   });
+
+  it('Should fit raw data to fourParamEq', () => {
+
+    const getFunction = ([a, b, c, d]) => (t => a + (b - a) / (1 + Math.pow(c, d) * Math.pow(t, -d)));
+    /*
+    const n = 154;
+    const xs = new Array(n).fill(0).map((zero, i) => -2.9 + i * (53 + 2.9) / (n - 1)); // [-2.9, ..., 53].length = n
+    const exactParameters = [2, 3, 5];
+    const data = {
+      x: xs,
+      y: xs.map(getFunction(exactParameters))
+    };
+    */
+    const data = {
+      x: [9.22e-12, 5.53e-11, 3.32e-10, 1.99e-9, 1.19e-8, 7.17e-8, 4.3e-7, 0.00000258, 0.0000155, 0.0000929],
+      y: [7.807, -3.74, 21.119, 2.382, 4.269, 41.57, 73.401, 98.535, 97.059, 92.147]
+    };
+
+    const options = {
+      damping: 0.0001,
+      maxIterations: 200,
+      minValues: [-Infinity, -Infinity, 0, -Infinity],
+      initialValues: [0, 100, 1, 0.1]
+    };
+
+    // TODO: Document where these values come from / why they are correct
+    const expectedParameters = [-32.0049530120509, 143.62056670443766, 1.8538886934094637e-7, 0.2715460299471644];
+    const actual = levenbergMarquardt(data, getFunction, options);
+    expect(actual.parameterValues).toBeDeepCloseTo(expectedParameters, 3);
+  });
 });
 
-test('fourParamEq', () => {
-  const options = {
-    damping: 0.0001,
-    maxIterations: 200,
-    minValues: [-Infinity, -Infinity, 0, -Infinity],
-    initialValues: [0, 100, 1, 0.1]
-  };
+describe('Handling of ill-behaved functions', () => {
+  it('Should throw an error if function evaluates to NaN', () => {
+    // TODO: Make this test unrelated to fourParamEq
+    const fourParamEq = ([a, b, c, d]) => (t => a + (b - a) / (1 + Math.pow(c, d) * Math.pow(t, -d)));
+    const data = {
+      x: [9.22e-12, 5.53e-11, 3.32e-10, 1.99e-9, 1.19e-8, 7.17e-8, 4.3e-7, 0.00000258, 0.0000155, 0.0000929],
+      y: [7.807, -3.74, 21.119, 2.382, 4.269, 41.57, 73.401, 98.535, 97.059, 92.147]
+    };
+    const options = {
+      damping: 0.01,
+      maxIterations: 200,
+      initialValues: [0, 100, 1, 0.1]
+      // these initialValues are OK but the increased damping option leads to a case where
+      // c < 0 && d is not an integer so Math.pow(c, d) is NaN
+    };
 
-  // TODO: Document where these values come from / why they are correct
-  expect(levenbergMarquardt(data, fourParamEq, options)).toBeDeepCloseTo({
-    parameterValues: [
-      -32.0049530120509,
-      143.62056670443766,
-      1.8538886934094637e-7,
-      0.2715460299471644
-    ],
-    residuals: 2913.145671022668,
-    iterations: 200,
-    parameterError: 2913.145671022668
-  }, 3);
-});
-
-test('error is NaN', () => {
-  const options = {
-    damping: 0.01,
-    maxIterations: 200,
-    initialValues: [0, 100, 1, 0.1]
-    // these initialValues are OK but the increased damping option leads to a case where
-    // c < 0 && d is not an integer so Math.pow(c, d) is NaN
-  };
-
-  try {
-    levenbergMarquardt(data, fourParamEq, options);
-  } catch (e) {
-    expect(e.message).toMatch('The function evaluated to NaN.');
-  }
+    try {
+      levenbergMarquardt(data, fourParamEq, options);
+    } catch (e) {
+      expect(e.message).toMatch('The function evaluated to NaN.');
+    }
+  });
 });
